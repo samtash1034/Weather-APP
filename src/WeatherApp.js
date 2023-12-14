34356a4368ddad37637827516cf7cc88575fa386
sam <samtash1034@gmail.com>
2023-12-14 08:22:49 +0800

    自定義hook

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,13 +1,9 @@
-import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import styled from '@emotion/styled';
-import { ReactComponent as AirFlowIcon } from './images/airFlow.svg';
-import { ReactComponent as RainIcon } from './images/rain.svg';
-import { ReactComponent as RefreshIcon } from './images/redo.svg';
-import WeatherIcon from './WeatherIcon';
 import sunriseAndSunsetData from './sunrise-sunset.json';
-import { ReactComponent as LoadingIcon } from './images/loading.svg';
-
+import WeatherCard from './WeatherCard';
 import { ThemeProvider } from '@emotion/react';
+import useWeatherApi from './useWeatherApi';
 
 const Container = styled.div`
   background-color: ${({ theme }) => theme.backgroundColor};
@@ -15,103 +11,6 @@ const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-`;
-
-const WeatherCard = styled.div`
-  position: relative;
-  min-width: 360px;
-  box-shadow: ${({ theme }) => theme.boxShadow};
-  background-color: ${({ theme }) => theme.foregroundColor};
-  box-sizing: border-box;
-  padding: 30px 15px;
-`;
-
-const Location = styled.div`
-  font-size: 28px;
-  color: ${({ theme }) => theme.titleColor};
-  margin-bottom: 20px;
-`;
-
-const Description = styled.div`
-  font-size: 16px;
-  color: ${({ theme }) => theme.textColor};
-  margin-bottom: 30px;
-`;
-
-const CurrentWeather = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 30px;
-`;
-
-const Temperature = styled.div`
-  color: ${({ theme }) => theme.temperatureColor};
-  font-size: 96px;
-  font-weight: 300;
-  display: flex;
-`;
-
-const Celsius = styled.div`
-  font-weight: normal;
-  font-size: 42px;
-`;
-
-const AirFlow = styled.div`
-  display: flex;
-  align-items: center;
-  font-size: 16x;
-  font-weight: 300;
-  color: ${({ theme }) => theme.textColor};
-  margin-bottom: 20px;
-
-  svg {
-    width: 25px;
-    height: auto;
-    margin-right: 30px;
-  }
-`;
-
-const Rain = styled.div`
-  display: flex;
-  align-items: center;
-  font-size: 16x;
-  font-weight: 300;
-  color: ${({ theme }) => theme.textColor};
-
-  svg {
-    width: 25px;
-    height: auto;
-    margin-right: 30px;
-  }
-`;
-
-const Refresh = styled.div`
-  position: absolute;
-  right: 15px;
-  bottom: 15px;
-  font-size: 12px;
-  display: inline-flex;
-  align-items: flex-end;
-  color: ${({ theme }) => theme.textColor};
-
-  svg {
-    margin-left: 10px;
-    width: 15px;
-    height: 15px;
-    cursor: pointer;
-    animation: rotate infinite 1.5s linear;
-    animation-duration: ${({ isLoading }) => (isLoading ? '1.5s' : '0s')};
-  }
-
-  @keyframes rotate {
-    from {
-      transform: rotate(360deg);
-    }
-    to {
-      transform: rotate(0deg);
-    }
-  }
 `;
 
 const theme = {
@@ -179,118 +78,17 @@ const getMoment = (locationName) => {
 
 const WeatherApp = () => {
   console.log('--- invoke function component ---');
-  const [weatherElement, setWeatherElement] = useState({
-    observationTime: new Date(),
-    locationName: '',
-    humid: 0,
-    temperature: 0,
-    windSpeed: 0,
-    description: '',
-    weatherCode: 0,
-    rainPossibility: 0,
-    comfortability: '',
-    isLoading: true,
-  });
 
-  const {
-    observationTime,
-    locationName,
-    temperature,
-    windSpeed,
-    description,
-    weatherCode,
-    rainPossibility,
-    comfortability,
-    isLoading,
-  } = weatherElement;
+  const [weatherElement, fetchData] = useWeatherApi();
 
-  // useCallback 的用法是將一個函式包覆並將該函式記憶起來，最後回傳記憶的函式
-  const fetchData = useCallback(() => {
-    const fetchingData = async () => {
-      // 使用 Promise.all 搭配 await 等待兩個 API 都取得回應後才繼續
-      const [currentWeather, weatherForecast] = await Promise.all([
-        fetchCurrentWeather(),
-        fetchWeatherForecast(),
-      ]);
-
-      setWeatherElement({
-        ...currentWeather,
-        ...weatherForecast,
-        isLoading: false,
-      });
-    };
-
-    setWeatherElement((prevState) => ({
-      ...prevState,
-      isLoading: true,
-    }));
-
-    fetchingData();
-    // 因為 fetchingData 沒有相依到 React 組件中的資料狀態，
-    // 所以 dependencies 陣列中不帶入元素
-  }, []); // dependincies 改變才會產生新的 fetchData
-
-  useEffect(() => {
-    console.log('execute function in useEffect');
-    fetchData();
-  }, [fetchData]); //如果沒有寫 useCallback 會造成無限迴圈
-  // (因為 fetchData function 是一個物件，物件指到的記憶體都不相同)
-
-  const fetchCurrentWeather = () => {
-    return fetch(
-      'https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=CWB-507B37E0-0383-4D8C-878D-628B54EC3536&StationName=新北'
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        const locationData = data.records.Station[0];
-
-        const weatherElements = {
-          WindSpeed: locationData.WeatherElement.WindSpeed,
-          AirTemperature: locationData.WeatherElement.AirTemperature,
-          RelativeHumidity: locationData.WeatherElement.RelativeHumidity,
-        };
-        return {
-          observationTime: locationData.ObsTime.DateTime,
-          locationName: locationData.StationName,
-          temperature: weatherElements.AirTemperature,
-          windSpeed: weatherElements.WindSpeed,
-          humid: weatherElements.RelativeHumidity,
-        };
-      });
-  };
-
-  const fetchWeatherForecast = () => {
-    return fetch(
-      'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-507B37E0-0383-4D8C-878D-628B54EC3536&locationName=臺北市'
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        const locationData = data.records.location[0];
-        const weatherElements = locationData.weatherElement.reduce(
-          (neededElements, item) => {
-            if (['Wx', 'PoP', 'CI'].includes(item.elementName)) {
-              neededElements[item.elementName] = item.time[0].parameter;
-            }
-            return neededElements;
-          },
-          {}
-        );
-        return {
-          description: weatherElements.Wx.parameterName,
-          weatherCode: weatherElements.Wx.parameterValue,
-          rainPossibility: weatherElements.PoP.parameterName,
-          comfortability: weatherElements.CI.parameterName,
-        };
-      });
-  };
+  // STEP 1：使用 useState 並定義 currentTheme 的預設值為 light
+  const [currentTheme, setCurrentTheme] = useState('dark');
 
   // STEP 3：透過 useMemo 避免每次都須重新計算取值，記得帶入 dependencies
   const moment = useMemo(
     () => getMoment(weatherElement.locationName),
     [weatherElement.locationName]
   );
-  // STEP 1：使用 useState 並定義 currentTheme 的預設值為 light
-  const [currentTheme, setCurrentTheme] = useState('dark');
 
   // 根據 moment 決定要使用亮色或暗色主題
   useEffect(() => {
@@ -302,39 +100,11 @@ const WeatherApp = () => {
     // 括號表示法，用變數來指定存取變數的名稱
     <ThemeProvider theme={theme[currentTheme]}>
       <Container>
-        {console.log('render', 'isLoading', isLoading)}
-        <WeatherCard>
-          <Location>{locationName}</Location>
-          <Description>
-            {description} {comfortability}
-          </Description>
-          <CurrentWeather>
-            <Temperature>
-              {Math.round(temperature)} <Celsius>°C</Celsius>
-            </Temperature>
-            <WeatherIcon
-              currentWeatherCode={weatherCode}
-              moment={moment || 'day'}
-            />
-          </CurrentWeather>
-          <AirFlow>
-            <AirFlowIcon />
-            {windSpeed} m/h
-          </AirFlow>
-          <Rain>
-            <RainIcon />
-            {Math.round(rainPossibility)} %
-          </Rain>
-
-          <Refresh onClick={fetchData} isLoading={isLoading}>
-            最後觀測時間：
-            {new Intl.DateTimeFormat('zh-TW', {
-              hour: 'numeric',
-              minute: 'numeric',
-            }).format(new Date(observationTime))}{' '}
-            {isLoading ? <LoadingIcon /> : <RefreshIcon />}
-          </Refresh>
-        </WeatherCard>
+        <WeatherCard
+          weatherElement={weatherElement}
+          moment={moment}
+          fetchData={fetchData}
+        />
       </Container>
     </ThemeProvider>
   );
---
+++ src/useWeatherApi.js
@@ -0,0 +1,100 @@
+import { useState, useEffect, useCallback } from 'react';
+
+const fetchCurrentWeather = () => {
+  return fetch(
+    'https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=CWB-507B37E0-0383-4D8C-878D-628B54EC3536&StationName=新北'
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const locationData = data.records.Station[0];
+
+      const weatherElements = {
+        WindSpeed: locationData.WeatherElement.WindSpeed,
+        AirTemperature: locationData.WeatherElement.AirTemperature,
+        RelativeHumidity: locationData.WeatherElement.RelativeHumidity,
+      };
+      return {
+        observationTime: locationData.ObsTime.DateTime,
+        locationName: locationData.StationName,
+        temperature: weatherElements.AirTemperature,
+        windSpeed: weatherElements.WindSpeed,
+        humid: weatherElements.RelativeHumidity,
+      };
+    });
+};
+
+const fetchWeatherForecast = () => {
+  return fetch(
+    'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-507B37E0-0383-4D8C-878D-628B54EC3536&locationName=臺北市'
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const locationData = data.records.location[0];
+      const weatherElements = locationData.weatherElement.reduce(
+        (neededElements, item) => {
+          if (['Wx', 'PoP', 'CI'].includes(item.elementName)) {
+            neededElements[item.elementName] = item.time[0].parameter;
+          }
+          return neededElements;
+        },
+        {}
+      );
+      return {
+        description: weatherElements.Wx.parameterName,
+        weatherCode: weatherElements.Wx.parameterValue,
+        rainPossibility: weatherElements.PoP.parameterName,
+        comfortability: weatherElements.CI.parameterName,
+      };
+    });
+};
+
+const useWeatherApi = () => {
+  const [weatherElement, setWeatherElement] = useState({
+    observationTime: new Date(),
+    locationName: '',
+    humid: 0,
+    temperature: 0,
+    windSpeed: 0,
+    description: '',
+    weatherCode: 0,
+    rainPossibility: 0,
+    comfortability: '',
+    isLoading: true,
+  });
+
+  // useCallback 的用法是將一個函式包覆並將該函式記憶起來，最後回傳記憶的函式
+  const fetchData = useCallback(() => {
+    const fetchingData = async () => {
+      // 使用 Promise.all 搭配 await 等待兩個 API 都取得回應後才繼續
+      const [currentWeather, weatherForecast] = await Promise.all([
+        fetchCurrentWeather(),
+        fetchWeatherForecast(),
+      ]);
+
+      setWeatherElement({
+        ...currentWeather,
+        ...weatherForecast,
+        isLoading: false,
+      });
+    };
+
+    setWeatherElement((prevState) => ({
+      ...prevState,
+      isLoading: true,
+    }));
+
+    fetchingData();
+    // 因為 fetchingData 沒有相依到 React 組件中的資料狀態，
+    // 所以 dependencies 陣列中不帶入元素
+  }, []); // dependincies 改變才會產生新的 fetchData
+
+  useEffect(() => {
+    console.log('execute function in useEffect');
+    fetchData();
+  }, [fetchData]); //如果沒有寫 useCallback 會造成無限迴圈
+  // (因為 fetchData function 是一個物件，物件指到的記憶體都不相同)
+
+  return [weatherElement, fetchData];
+};
+
+export default useWeatherApi;
